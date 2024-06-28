47510afe44e0f4e70e96e11e61334ded6aed326b
Regina (Reg) Chitralla <reginachitralla@gmail.com>
2024-06-28 11:01:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -103,11 +103,12 @@ def display_questions():
     responses = []
     for item in st.session_state['shuffled_questions']:
         st.write(item["question"])
+        score = st.session_state.get(f"{item['category']}_{item['type']}_{item['question']}", None)
         cols = st.columns(5)
-        score = None
         for i, col in enumerate(cols):
             if col.radio("", [i+1], key=f"{item['category']}_{item['type']}_{item['question']}_{i}"):
                 score = i + 1
+                st.session_state[f"{item['category']}_{item['type']}_{item['question']}"] = score
         responses.append({
             "category": item["category"],
             "type": item["type"],
