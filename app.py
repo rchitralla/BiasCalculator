6a29ec13504089141246e90987d1e806cf1ea1d6
Regina (Reg) Chitralla <reginachitralla@gmail.com>
2024-06-28 10:29:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,83 @@ logo_path = "Logo.png"
 
 # Define the categories, types, and questions
 categories = {
-    # Your categories dictionary here...
+    "Culture & Engagement": {
+        "Individual Actions": [
+            "I use language carefully to avoid terms that may be degrading or hurtful."
+        ],
+        "Institution Actions": [
+            "I support reviewing policies across all functions to ensure they are inclusive and free from bias."
+        ]
+    },
+    "Development": {
+        "Individual Actions": [
+            "I sponsor and mentor employees from historically marginalized groups.",
+            "I create individual development plans for every team member.",
+            "I hold my team accountable for mentoring and sponsoring marginalized employees, including in performance reviews."
+        ],
+        "Industry Actions": [
+            "I mentor and sponsor people from marginalized groups within and outside my organization."
+        ],
+        "Institution Actions": [
+            "I mentor and sponsor people from marginalized groups within and outside my organization.",
+            "I monitor training participation to ensure inclusion of all backgrounds.",
+            "I support formal mentoring and sponsorship programs in my organization.",
+            "I support ongoing inclusion/unconscious bias training for all employees."
+        ]
+    },
+    "Exit & Retain": {
+        "Individual Actions": [
+            "I personally speak to critical employees from all backgrounds to understand their exit and stay reasons."
+        ],
+        "Institution Actions": [
+            "My function conducts regular exit interviews.",
+            "My function takes actions to improve retention of people from all backgrounds."
+        ]
+    },
+    "General": {
+        "Individual Actions": [
+            "I speak up when team members say things based on stereotypes or assumptions.",
+            "I build strong partnerships with communities and organizations supporting historically marginalized groups.",
+            "I give equal attention to people from all backgrounds.",
+            "I value dissenting opinions, even when they make me uncomfortable.",
+            "I examine my connections and diversify the perspectives and experiences around me.",
+            "I consider multiple data sources and avoid relying on 'gut reaction' for decisions."
+        ],
+        "Institution Actions": [
+            "I review the team for diversity when starting a project and take action if it's not diverse.",
+            "I encourage my team to speak up against stereotypes or assumptions.",
+            "I promote the sharing of dissenting opinions across the team.",
+            "I seek insights from Employee Resource Groups to improve my function/team/department."
+        ],
+        "Industry Actions": [
+            "I build strong partnerships with communities supporting historically marginalized groups."
+        ]
+    },
+    "Performance & Reward": {
+        "Individual Actions": [
+            "I regularly review and address bias/equity in pay decisions.",
+            "During performance reviews, I check ratings distributions by demographic for potential bias."
+        ],
+        "Institution Actions": [
+            "I support the systemic review of pay equity and performance ratings by demographic annually."
+        ],
+        "Industry Actions": [
+            "I support publicly sharing pay equity results and plans to address gaps."
+        ]
+    },
+    "Recruiting & Hiring": {
+        "Individual Actions": [
+            "I wait to decide on hiring until there is a balanced slate of candidates.",
+            "I use structured interview guides and consistent criteria for all candidates.",
+            "I use structured guides and consistent criteria for all interviews.",
+            "Every new team member takes inclusion/unconscious bias training when starting."
+        ],
+        "Institution Actions": [
+            "My function has a balanced slate policy.",
+            "My function requires structured interviews or diverse panels for all roles.",
+            "My function includes inclusion/unconscious bias training in new hire onboarding."
+        ]
+    }
 }
 
 # Flattened list of questions to display without headers
@@ -41,10 +117,14 @@ def calculate_total_score(responses):
     return total_score
 
 # Function to calculate the total score per category
-def calculate_total_scores_per_category(total_scores):
+def calculate_total_scores_per_category(responses):
     total_scores_per_category = {}
-    for category_name, types in total_scores.items():
-        total_scores_per_category[category_name] = sum(types.values())
+    for response in responses:
+        category = response["category"]
+        score = response["score"]
+        if category not in total_scores_per_category:
+            total_scores_per_category[category] = 0
+        total_scores_per_category[category] += score
     return total_scores_per_category
 
 # Function to calculate the maximum possible score per category
@@ -58,7 +138,7 @@ def calculate_max_scores_per_category(categories):
 # Main function to display the self-assessment form
 def main():
     st.image(logo_path, width=200)  # Add your logo at the top
-    st.title("Anti-Bias Self Assessment Tool")
+    st.title("Self Assessment Tool")
     st.write(
         "This tool enables you to explore your own behaviors related to bias & inclusion in the workplace. "
         "Your results are yours and yours alone -- they will not be submitted or shared in any manner unless you choose to do so."
@@ -78,23 +158,11 @@ def main():
     # Display the questions and collect responses
     responses = display_questions()
 
-    # Group responses by category and type for analytics
-    total_scores = {}
-    for response in responses:
-        category = response["category"]
-        type_ = response["type"]
-        score = response["score"]
-        if category not in total_scores:
-            total_scores[category] = {}
-        if type_ not in total_scores[category]:
-            total_scores[category][type_] = 0
-        total_scores[category][type_] += score
-
     # Calculate the total score
     total_score = calculate_total_score(responses)
 
     # Calculate the total scores per category
-    total_scores_per_category = calculate_total_scores_per_category(total_scores)
+    total_scores_per_category = calculate_total_scores_per_category(responses)
 
     # Calculate the maximum possible scores per category
     max_scores_per_category = calculate_max_scores_per_category(categories)
@@ -103,24 +171,13 @@ def main():
     if st.button("Submit"):
         st.write("## Assessment Complete. Here are your results:")
 
-        # Display grouped results
-        for category_name, types in total_scores.items():
-            for type_name, score in types.items():
-                st.write(f"**{category_name} - {type_name}: {score}**")
-
-        # Display total score
-        st.write(f"### Total Score: {total_score}")
-
         # Display total scores per category
         for category_name, score in total_scores_per_category.items():
-            st.write(f"**{category_name}: {score} out of {max_scores_per_category[category_name]}**")
-
-        # Display progress bars for each category
-        st.write("## Progress Bars for Each Category:")
-        for category_name, score in total_scores_per_category.items():
             max_score = max_scores_per_category[category_name]
+            st.write(f"**{category_name}: {score} out of {max_score}**")
+
+            # Calculate and display progress bar
             progress = int((score / max_score) * 100)
-            st.write(f"**{category_name}:**")
             st.progress(progress)
 
         # Prepare data for visualization
