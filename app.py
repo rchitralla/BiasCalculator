6fdd8cb6d915ef78837dff794f44d9e6e211746d
Regina (Reg) Chitralla <reginachitralla@gmail.com>
2024-06-28 12:01:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -103,8 +103,13 @@ def display_questions():
     responses = []
     for item in st.session_state['shuffled_questions']:
         st.write(item["question"])
+        cols = st.columns(5)
         options = ["1", "2", "3", "4", "5"]
-        selected_option = st.radio("", options, index=2, key=f"{item['category']}_{item['type']}_{item['question']}")
+        selected_option = st.session_state.get(f"{item['category']}_{item['type']}_{item['question']}", "3")
+        for i, col in enumerate(cols):
+            if col.radio("", [options[i]], index=0 if selected_option == options[i] else -1, key=f"{item['category']}_{item['type']}_{item['question']}_{options[i]}"):
+                selected_option = options[i]
+        st.session_state[f"{item['category']}_{item['type']}_{item['question']}"] = selected_option
         responses.append({
             "category": item["category"],
             "type": item["type"],
@@ -210,22 +215,6 @@ def main():
         fig_doughnut = px.pie(scores_data, names='Category', values='Score', title='Score Distribution by Category',
                               hole=0.4, color_discrete_sequence=["#377bff", "#15965f", "#fa6868"])
         st.plotly_chart(fig_doughnut)
-
-# Add CSS to style radio buttons horizontally
-st.markdown(
-    """
-    <style>
-    div[data-baseweb="radio"] > div {
-        display: flex;
-        flex-direction: row;
-    }
-    div[data-baseweb="radio"] label {
-        margin-right: 10px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 if __name__ == "__main__":
     main()
